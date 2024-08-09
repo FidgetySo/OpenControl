c9a71744fdbb1b4825d1de3f63e97f7540c658be
Elijah V. Schrotenboer <elijahschrotenboer@gmail.com>
2024-08-08 23:05:54 -0400

    Update client_relay.py

--- Python Source/relay/client_relay.py
+++ Python Source/relay/client_relay.py
@@ -5,20 +5,3 @@ class ClientRelay:
 		self.config = config
 
 		self.port = config['Connection']['RelayPort']
-	def send_pac
-	def send_crafting(self, item, amount=1, push=False):
-		"""
-		Steps
-		1. Generate Packet String
-		2. Send Packet String to relay server
-			a. Relay server recieves packet
-		3. Recieve Response Packet
-		4. 
-		"""
-		# Generate Packet String
-		packet_str =
-		# Send Packet and Recieve Packet
-		reponse =
-
-	def recieve_crafting(self):
-		return response_code, item, remaining,  
